3bb044b2ff1291f25be55664ab1a2aa6d5e921c0
Cesar Pereira <cesar.pereiram@gmail.com>
2023-09-24 20:33:23 -0300

    Primeira Classe Categoria

--- src/main/java/com/cesarpereira/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cesarpereira/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,31 @@
 package com.cesarpereira.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cesarpereira.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "Reste Funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		Categoria cat3 = new Categoria(3, "Eletronicos");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat3);
+		lista.add(cat2);
+		
+		return lista;
 		
 	}
 	
