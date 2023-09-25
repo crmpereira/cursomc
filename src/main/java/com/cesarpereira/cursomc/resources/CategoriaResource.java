e125b940f33fb8a3ebd1e8d388562a1ed4caedc7
crmpereira <cesar.pereiram@gmail.com>
2023-09-25 20:33:58 -0300

    Banco PostGree

--- src/main/java/com/cesarpereira/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cesarpereira/cursomc/resources/CategoriaResource.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.cesarpereira.cursomc.domain.Categoria;
 
 @RestController
