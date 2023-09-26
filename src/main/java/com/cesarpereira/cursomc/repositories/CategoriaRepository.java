5b0999e1c03247353d9863fcf9f3579bd1e59650
crmpereira <cesar.pereiram@gmail.com>
2023-09-25 21:32:03 -0300

    Service/Repository

---
+++ src/main/java/com/cesarpereira/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package com.cesarpereira.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.cesarpereira.cursomc.domain.Categoria;
+
+
+
+public interface CategoriaRepository  extends JpaRepository <Categoria,Integer> {
+
+	
+	
+	
+}
+
